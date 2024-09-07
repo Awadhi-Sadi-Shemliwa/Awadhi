314e6d655bccd75ff8e4e486ecebf9cae161f512
Awadhi-Sadi-Shemliwa <124035176+Awadhi-Sadi-Shemliwa@users.noreply.github.com>
2024-09-07 13:00:32 +0300

    Add files via upload

--- SHEDA/js/main.js
+++ SHEDA/js/main.js
@@ -36,72 +36,72 @@
     });
 
 
-    // Latest-news-carousel
-    $(".latest-news-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-                items:4
-            }
-        }
-    });
+    // // Latest-news-carousel
+    // $(".latest-news-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 2000,
+    //     center: false,
+    //     dots: true,
+    //     loop: true,
+    //     margin: 25,
+    //     nav : true,
+    //     navText : [
+    //         '<i class="bi bi-arrow-left"></i>',
+    //         '<i class="bi bi-arrow-right"></i>'
+    //     ],
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         576:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         },
+    //         992:{
+    //             items:3
+    //         },
+    //         1200:{
+    //             items:4
+    //         }
+    //     }
+    // });
 
 
-    // What's New carousel
-    $(".whats-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
+    // // What's New carousel
+    // $(".whats-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 2000,
+    //     center: false,
+    //     dots: true,
+    //     loop: true,
+    //     margin: 25,
+    //     nav : true,
+    //     navText : [
+    //         '<i class="bi bi-arrow-left"></i>',
+    //         '<i class="bi bi-arrow-right"></i>'
+    //     ],
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         576:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         },
+    //         992:{
+    //             items:2
+    //         },
+    //         1200:{
+    //             items:2
+    //         }
+    //     }
+    // });
 
 
 
@@ -125,4 +125,7 @@
 
 
 })(jQuery);
+
+
+
 
